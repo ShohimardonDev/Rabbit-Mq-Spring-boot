855afce34fb6de4f549e1112e36702c28a276de9
Shohkimardon <abdurashitovsh19@gmail.com>
2025-09-10 11:10:19 +0500

    init

---
+++ src/main/java/uz/ssh/rabbitmqspringboot/RabbitMqSpringBootApplication.java
@@ -0,0 +1,13 @@
+package uz.ssh.rabbitmqspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqSpringBootApplication.class, args);
+    }
+
+}
